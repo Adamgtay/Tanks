8e8dd7feadfc37a9f8de8d6027db4c221502528a
Adamgtay <adamgtaylor26@gmail.com>
2024-02-17 23:22:19 +0200

    more setup files

---
+++ cmd/tanks.py
@@ -0,0 +1,8 @@
+import pygame
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from internal.pkg.constants import constants
+from pygame import mixer
+
+print(constants.FPS)
---
+++ internal/pkg/constants/constants.py
@@ -0,0 +1,7 @@
+
+
+
+# frames per second
+FPS = 60
+
+FONT_LOCATION = "/Users/Adam/Desktop/coding/pfe/tanks/tanks/dogica.ttf"
