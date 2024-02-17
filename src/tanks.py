3c14970ed06761ff5ee679e266a78fae764e0e79
Adamgtay <adamgtaylor26@gmail.com>
2024-02-17 23:35:12 +0200

    folder restructure

--- src/tanks.py
+++ src/tanks.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from internal.pkg.constants import constants
+from src.game_logic import constants
 from pygame import mixer
 
 print(constants.FPS)
