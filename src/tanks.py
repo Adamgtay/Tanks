f7974399ab0e0c48923b1bb44c5abd85e4a14ddd
Adamgtay <adamgtaylor26@gmail.com>
2024-02-18 14:37:01 +0200

    updated init

--- src/tanks.py
+++ src/tanks.py
@@ -1,8 +1,5 @@
 import pygame
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.game_logic import constants
+from game_logic import constants
 from pygame import mixer
 
 print(constants.FPS)
