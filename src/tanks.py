97f2da2b4ece6f5f157f986302a55f74451d68f8
Adamgtay <adamgtaylor26@gmail.com>
2024-02-21 15:37:11 +0200

    ascii stuff

--- src/tanks.py
+++ src/tanks.py
@@ -1,14 +1,14 @@
 import pygame
-from game_logic import constants
 from game_logic.ascii_graphics import ascii_tanks
-from game_logic import event_handler
 from game_logic.levels import level_one
 from game_logic import load_music
-from pygame import mixer
 
 pygame.init()
 
 load_music.load_music("/Users/Adam/Desktop/coding/games/Tanks/assets/sound/main-theme.wav")
+
+# Set up font
+ascii_font = pygame.font.Font("/Users/Adam/Desktop/coding/games/Tanks/assets/fonts/dogica.ttf", 36)
 
 game_running = True
 
