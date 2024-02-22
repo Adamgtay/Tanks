179c9c02a47331d21ef1a4d25d3fa7c6e9527ca2
Adamgtay <adamgtaylor26@gmail.com>
2024-02-22 09:05:02 +0200

    effective exit solution from any screen

--- src/game_logic/event_handler.py
+++ src/game_logic/event_handler.py
@@ -1,10 +1,13 @@
-import pygame
+import pygame,sys
 from game_logic.levels import level_1
 
 def event_handler_startup(game_running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
+            # Quit Pygame
+            pygame.quit()
+            sys.exit()
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     level_1.level_one(game_running)    
@@ -15,6 +18,9 @@ def event_handler_level_one(game_running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
+            # Quit Pygame
+            pygame.quit()
+            sys.exit()
     return game_running
 
   
--- src/tanks.py
+++ src/tanks.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,sys
 from game_logic import load_music
 from game_logic.levels import startup_screen
 from pygame import mixer
@@ -6,10 +6,11 @@ from pygame import mixer
 pygame.init()
 pygame.font.init()
 pygame.display.init()
-
-
-
 load_music.load_music("/Users/Adam/Desktop/coding/games/Tanks/assets/sound/main-theme.wav")
 game_running = True
 
 startup_screen.startup_screen(game_running)
+
+# Quit Pygame
+pygame.quit()
+sys.exit()
